db57e1a2f43ff86998a9b113b6f92c5e44d6a069
root <root@localhost.localdomain>
2017-05-13 15:26:45 +0900

    [change] change http

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-var http = require('http');
-var socketio = require('socket.io');
-var fs = require('fs');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).listen(process.env.PORT || 3000);  // ポート競合の場合は値を変更
- 
-var io = socketio.listen(server);
- 
-io.sockets.on('connection', function(socket) {
-   
-    socket.on('test', function(data) {
-        io.sockets.emit('test_back', {value : data.value});
-        console.log("a");
-    });
+var http = require( 'http' ); // HTTPモジュール読み込み
+var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
+var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
+var pg = require( 'pg' );
+var id;
+
+//サーバー実装の前にエラーハンドリングを記述
+process.on('uncaughtException', function(err) {
+  console.log(err);
 });
+
+/*
+//ちゃんとエラー処理を書くとこうなる
+try {
+  var json = JSON.parse('<tag>NOT JSON FORMART</tag>');
+} catch (e) {
+  res.writeHead(400, {"Content-Type":"text/html"});
+  res.end('invalid format');
+  return;
+}
+*/
+
+// ポート固定でHTTPサーバーを立てる
+var server = http.createServer( function( req, res ) {
+  //もしURLにファイル名がないならばindex.htmlに飛ばすように
+  if(req.url == "/")
+    req.url = "/index.html";
+  //URLでリクエストされたページをread
+  fs.readFile(__dirname + req.url, 'utf-8', function(err, data){
+    //もし見つからなかったら404を返す
+    if(err){　//err=trueならNot Foundを返します。
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write("Not Found");
+      return res.end();　
+    }
+    //見つかったら表示
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+});
+server.listen(process.env.PORT)
