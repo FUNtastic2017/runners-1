40f3d8029ce1665a830e77bb589b74345da1ab04
root <root@localhost.localdomain>
2017-05-13 16:08:56 +0900

    [change] reverse all

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-var http = require( 'http' ); // HTTPモジュール読み込み
-var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
-var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
-var pg = require( 'pg' );
-var id;
-
-//サーバー実装の前にエラーハンドリングを記述
-process.on('uncaughtException', function(err) {
-  console.log(err);
-});
-
-// ポート固定でHTTPサーバーを立てる
-var server = http.createServer( function( req, res ) {
-  //もしURLにファイル名がないならばindex.htmlに飛ばすように
-  if(req.url == "/")
-    req.url = "/index.html";
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-  });
-
-server.listen(process.env.PORT)
-
-var io = socketio.listern(server);
-
+}).listen(process.env.PORT || 3000);  // ポート競合の場合は値を変更
+ 
+var io = socketio.listen(server);
+ 
 io.sockets.on('connection', function(socket) {
-
-	socket.on('test', function(data) {
-	 io.sockets.emit('test_back', {value : data.value});
-	 console.log("debug");
-	});
+   
+    socket.on('test', function(data) {
+        io.sockets.emit('test_back', {value : data.value});
+        console.log("a");
+    });
 });
