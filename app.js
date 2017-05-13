6fbe21e0a4e9d17f32da4e92f3d2fb283b9e03bf
root <root@localhost.localdomain>
2017-05-13 16:00:43 +0900

    [change] fix http

--- app.js
+++ app.js
@@ -9,36 +9,16 @@ process.on('uncaughtException', function(err) {
   console.log(err);
 });
 
-/*
-//ちゃんとエラー処理を書くとこうなる
-try {
-  var json = JSON.parse('<tag>NOT JSON FORMART</tag>');
-} catch (e) {
-  res.writeHead(400, {"Content-Type":"text/html"});
-  res.end('invalid format');
-  return;
-}
-*/
-
 // ポート固定でHTTPサーバーを立てる
 var server = http.createServer( function( req, res ) {
   //もしURLにファイル名がないならばindex.htmlに飛ばすように
   if(req.url == "/")
     req.url = "/index.html";
-  //URLでリクエストされたページをread
-  fs.readFile(__dirname + req.url, 'utf-8', function(err, data){
-    //もし見つからなかったら404を返す
-    if(err){　//err=trueならNot Foundを返します。
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write("Not Found");
-      return res.end();　
-    }
-    //見つかったら表示
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-});
+
 server.listen(process.env.PORT)
 
 var io = socketio.listern(server);
